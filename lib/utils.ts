a8911f86218f544e6a6d2ff38e11ee34e7872c1c
Sage Baram <sagebaram.career@gamil.com>
2024-06-03 16:13:01 +0300

    (feat) add auth form, user actions for auth, appwrite baas

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,17 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+  firstName: type === "sign-in" ? z.string().optional() : z.string().min(2).max(20),
+  lastName: type === "sign-in" ? z.string().optional() : z.string().min(2),
+  address1: type === "sign-in" ? z.string().optional() : z.string().min(2).max(40),
+  city: type === "sign-in" ? z.string().optional() : z.string().min(2).max(40),
+  state: type === "sign-in" ? z.string().optional() : z.string().min(2).max(40),
+  postalCode: type === "sign-in" ? z.string().optional() : z.string().min(4),
+  dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
+  ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
+
+  email: z.string().email(),
+  password: z.string().min(8),
+})
